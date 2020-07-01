dafbd292a98750806730adffe5dbf2f687f15352
kavinrajsi <we@sikavinraj.me>
2020-07-01 21:23:46 +0530

    updated the js as per lint, key modification for project

--- app/scripts/project/avishkaram/main.js
+++ app/scripts/project/avishkaram/main.js
@@ -1,12 +1,12 @@
-let motivesListHead = document.querySelectorAll('.motives_list-head');
-let motivesListIcon = document.querySelectorAll('.motives_list-icon');
-for(let i =0; i <= motivesListHead.length ; i++){
-    motivesListHead[i].addEventListener('mouseover', function(){
-        motivesListIcon[i].classList.add('motives_list_icon-show');
-    });
-    motivesListHead[i].addEventListener('mouseout', function(){
-        motivesListIcon[i].classList.remove('motives_list_icon-show');
-    });
+const motivesListHead = document.querySelectorAll('.motives_list-head');
+const motivesListIcon = document.querySelectorAll('.motives_list-icon');
+for (let i =0; i <= motivesListHead.length; i++) {
+  motivesListHead[i].addEventListener('mouseover', function() {
+    motivesListIcon[i].classList.add('motives_list_icon-show');
+  });
+  motivesListHead[i].addEventListener('mouseout', function() {
+    motivesListIcon[i].classList.remove('motives_list_icon-show');
+  });
 }
 
 
